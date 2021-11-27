3f5ced3dc2713f32a1fd2e97ce85fc1a3bbc06a4
wasif789 <wsffarid1@gmail.com>
2021-11-28 00:47:40 +0530

    Create Table

--- Address_Book_SQL.sql
+++ Address_Book_SQL.sql
@@ -3,3 +3,16 @@
 create database AddressBookSystem;
 
 use AddressBookSystem;
+-----------------UC2 Create Table---------------
+
+create table Address_Book_Table
+(
+FirstName varchar(50) not null,
+LastName varchar(20),
+Address varchar(100),
+City varchar(50),
+StateName varchar(55),
+ZipCode int,
+Phonenum bigint,
+EmailId varchar(100)
+);
